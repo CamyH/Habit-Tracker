b03ba66282c1069d4f3ee8266175155ec5fcf12f
CamyH <cameronhunt88@hotmail.com>
2024-09-14 21:15:33 +0100

    Refactor to use external styles

--- habit-tracker/app/+not-found.tsx
+++ habit-tracker/app/+not-found.tsx
@@ -1,40 +1,25 @@
-import { Link, Stack } from 'expo-router';
-import { StyleSheet } from 'react-native';
+// Hooks
+import { Link, Stack } from "expo-router"
 
-import { Text, View } from '@/components/Themed';
+// Components
+import { Text, View } from "@/components/Themed"
+
+// Styles
+import { useStyles } from "@/styles/not-found.styles"
 
 export default function NotFoundScreen() {
+  const styles = useStyles
+
   return (
     <>
-      <Stack.Screen options={{ title: 'Oops!' }} />
+      <Stack.Screen options={{ title: "Oops!" }} />
       <View style={styles.container}>
         <Text style={styles.title}>This screen doesn't exist.</Text>
 
-        <Link href="/" style={styles.link}>
+        <Link href='/' style={styles.link}>
           <Text style={styles.linkText}>Go to home screen!</Text>
         </Link>
       </View>
     </>
-  );
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 20,
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  link: {
-    marginTop: 15,
-    paddingVertical: 15,
-  },
-  linkText: {
-    fontSize: 14,
-    color: '#2e78b7',
-  },
-});
