c37c650b59f71c141ba2951dfe0fbc9d205cc15f
CamyH <cameronhunt88@hotmail.com>
2024-09-14 21:03:31 +0100

    Refactor index page to use external stylesheet and color const

--- habit-tracker/app/(tabs)/index.tsx
+++ habit-tracker/app/(tabs)/index.tsx
@@ -1,31 +1,23 @@
-import { StyleSheet } from 'react-native';
+// Components
+import EditScreenInfo from "@/components/EditScreenInfo"
+import { Text, View } from "@/components/Themed"
+import Colors from "@/constants/Colors"
 
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
+// Styles
+import { useStyles } from "@/styles/index.styles"
 
 export default function TabOneScreen() {
+  const styles = useStyles
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <View
+        style={styles.separator}
+        lightColor={Colors.light.background}
+        darkColor={Colors.dark.background}
+      />
+      <EditScreenInfo path='app/(tabs)/index.tsx' />
     </View>
-  );
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
