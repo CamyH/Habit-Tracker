85eeda400dccd8fef5e56de8ad0384b68b646acf
CamyH <cameronhunt88@hotmail.com>
2024-09-15 19:21:20 +0100

    Refactor to use external stylesheet

---
+++ habit-tracker/app/(tabs)/stats.tsx
@@ -0,0 +1,25 @@
+// Components
+import EditScreenInfo from "@/components/EditScreenInfo"
+import { Text, View } from "@/components/Themed"
+
+// Constants
+import PageText from "@/constants/PageText"
+import Colors from "@/constants/Colors"
+
+// Styles
+import { useStyles } from "@/styles/stats.styles"
+
+export default function StatsScreen() {
+  const styles = useStyles
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>{PageText.stats.pageHeader}</Text>
+      <View
+        style={styles.separator}
+        lightColor={Colors.light.background}
+        darkColor={Colors.dark.background}
+      />
+      <EditScreenInfo path='app/(tabs)/two.tsx' />
+    </View>
+  )
+}
